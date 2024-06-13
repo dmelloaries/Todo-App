1f5ea5b7a11fa683f144bfd5721e46ddbb35b4c0
Aries Dmello <dmelloaries@gmail.com>
2024-06-14 00:42:55 +0530

    Frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import {Createtodo} from './components/Createtodo.jsx';
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <Createtodo></Createtodo>
+
+
+
+    </div>
+    
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Createtodo.jsx
@@ -0,0 +1,14 @@
+//import React from 'react'
+
+export const Createtodo = () => {
+  return (
+    <div>
+        <input type="text" placeholder='Title'></input><br/>
+        <input type="text" placeholder='Description'></input><br/>
+        <button>Add Todo</button>
+      
+    </div>
+  )
+}
+
+
