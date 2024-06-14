f4754b321b817dac05612cc384b6765cdf55f281
Aries Dmello <dmelloaries@gmail.com>
2024-06-14 17:01:59 +0530

    completed backend and some frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,24 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import {Createtodo} from './components/Createtodo.jsx';
+import { useState } from 'react'
 
+import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
+
+// useEffect hook
 function App() {
-  //const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  // fetch("http://localhost:3000/todos")
+  //   .then(async function(res) {
+  //     const json = await res.json();
+  //     setTodos(json.todos);
+  //   })
 
   return (
-    <>
     <div>
-      <Createtodo></Createtodo>
-
-
-
+      <CreateTodo></CreateTodo>
+      <Todos todos={todos}></Todos>
     </div>
-    
-    
-      
-    </>
   )
 }
 
